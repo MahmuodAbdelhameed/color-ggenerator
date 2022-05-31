4315fb42313778071b8985533b58a318270e224d
MahmuodAbdelhameed <54806019+MahmuodAbdelhameed@users.noreply.github.com>
2022-06-01 01:02:29 +0200

    Add files via upload

---
+++ hex.js
@@ -0,0 +1,16 @@
+const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A','B', 'C', 'D', 'E', "f",];
+// #f15025
+const btn = document.querySelector('.btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function(){
+    let hexColor = '#';
+    for(let i = 0; i < 6; i++){
+        hexColor += hex[getRandomNumber()];
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+})
+function getRandomNumber(){
+    return Math.floor(Math.random() * hex.length);
+}
